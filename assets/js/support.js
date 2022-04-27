064d2663277849d2ffc2bf03dad3d90fe6279916
Novatify Technology <adeolu.gideon>
2022-04-27 12:19:57 +0100

    added support system and share button

---
+++ assets/js/support.js
@@ -0,0 +1,10 @@
+
+    var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+    (function () {
+        var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
+        s1.async = true;
+        s1.src = 'https://embed.tawk.to/6269218d7b967b11798cc2c1/1g1lam4sd';
+        s1.charset = 'UTF-8';
+        s1.setAttribute('crossorigin', '*');
+        s0.parentNode.insertBefore(s1, s0);
+    })();
