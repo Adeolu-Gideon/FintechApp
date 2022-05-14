d6a8da693dc5367e7f90c702f08a0f18736209e0
Novatify Technology <adeolu.gideon>
2022-05-14 19:04:01 +0100

    connected to heroku database

--- includes/db.php
+++ includes/db.php
@@ -1,9 +1,12 @@
 <?php
-    // Enter your host name, database username, password, and database name.
-    // If you have not set database password on localhost then set empty.
-    $conn = mysqli_connect("localhost","root","","fintechapp");
-    // Check connection
-    if (mysqli_connect_errno()){
-        echo "Failed to connect to MySQL: " . mysqli_connect_error();
-    }
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("mysql://b8fa625e1c66cf:2cb66deb@us-cdbr-east-05.cleardb.net/heroku_0d5ab19cf28af2d?reconnect=true"));
+$cleardb_server = $cleardb_url["us-cdbr-east-05.cleardb.net"];
+$cleardb_username = $cleardb_url["b8fa625e1c66cf"];
+$cleardb_password = $cleardb_url["2cb66deb"];
+$cleardb_db = substr($cleardb_url["heroku_0d5ab19cf28af2d"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 ?>
